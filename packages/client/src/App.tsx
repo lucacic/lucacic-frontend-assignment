a0fd5e6a4e40b85786010ea59cc54a63398a4d08
luca cicciotti <l.cicciotti@gmail.com>
2021-02-20 19:49:37 +0100

    Add Webpack environment and React and Typescript

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function App()
+{
+    return <h1 className="test">Hello, worldd!</h1>
+}
---
+++ packages/client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    output: {
+        filename: 'main.[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
+}
