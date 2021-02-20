006aecf9f9bea6605ef184d5080c458bc1e53826
luca cicciotti <l.cicciotti@gmail.com>
2021-02-20 21:36:53 +0100

    Add Ant Design UI library and CSS Loader for webpack

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
+import { Button } from '../node_modules/antd/lib/index';
+import 'antd/dist/antd.css';
+
 export default function App()
 {
-    return <h1 className="test">Hello, worldd!</h1>
+    return (
+        <>
+        <Button type="primary">Button</Button>
+        <h1 className="test">Hello, worldd!</h1>
+        </>
+    )
 }
